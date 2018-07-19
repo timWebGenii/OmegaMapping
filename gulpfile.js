452e3c20e4c43694a72c85e861151195d40d2b6e
timWebsiteGenii <tim@websitegenii.com>
2018-07-19 15:09:53 -0400

    myFinal

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
+var injectPartials = require('gulp-inject-partials');
 var browserSync = require('browser-sync').create();
 
 
@@ -32,23 +33,41 @@ gulp.task('sass', function () {
   gulp.src('./sass/**/*.scss')
     .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(autoprefixer('last 2 version'))
+        .pipe(autoprefixer({
+            browsers: ['last 7 versions'],
+            cascade: false
+        }))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./css'))
-    .pipe(browserSync.reload({
-		stream: true
-    }))
+    .pipe(browserSync.stream());
 });
 
+gulp.task('serve', ['sass'], function() {
 
-gulp.task('watch',['browserSync', 'sass'], function(){
-    gulp.watch('./sass/**/*.scss', ['sass']).on('change', browserSync.reload);
-    gulp.watch('css/main.css').on('change', browserSync.reload);
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("app/scss/*.scss", ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('watch',['sass','browserSync' ], function(){
+    gulp.watch('./sass/**/*.scss', ['sass']);
     gulp.watch('**/*.html').on('change', browserSync.reload);
     
 });
 
+gulp.task('default', ['watch']);
 
 gulp.task('build', function(){
     runSequence('sass', 'imagemin');
 });
+
+
+// partial inject right into html need to add remove tag
+gulp.task('index', function(){
+    return gulp.src('./contact.html')
+        .pipe(injectPartials())
+        .pipe(gulp.dest('./'));
+})
